4e88736962434287b1ee0bd3f528f2ff8e140c2b
Siwei Cao <siwei@Siweis-MacBook-Pro.local>
2023-11-18 16:27:12 -0500

    test

--- tests/testthat.R
+++ tests/testthat.R
@@ -9,4 +9,67 @@
 library(testthat)
 library(linreg)
 
-test_check("linreg")
+linreg_model=linreg(Temp~Wind+Month+Ozone, airquality)
+lm_model=lm(Temp~Wind+Month+Ozone, airquality)
+lm_sum=summary(lm_model)
+linregsum=linreg_sum(Temp~Wind+Month+Ozone, airquality)
+
+test_that("linreg works", {
+  expect_equal(
+    linreg_model$coefficients,
+    lm_model$coefficients
+  )
+  expect_equal(
+    linreg_model$residuals,
+    lm_model$residuals
+  )
+  expect_equal(
+    linreg_model$rank,
+    lm_model$rank
+  )
+  expect_equal(
+    linreg_model$fitted.values,
+    lm_model$fitted.values
+  )
+  expect_equal(
+    linreg_model$df.residual,
+    lm_model$df.residual
+  )
+})
+
+test_that("linreg_sum works", {
+  expect_equal(
+    linregsum$coefficients,
+    lm_sum$coefficients
+  )
+  expect_equal(
+    linregsum$sigma,
+    lm_sum$sigma
+  )
+  expect_equal(
+    linregsum$r.squared,
+    lm_sum$r.squared
+  )
+  expect_equal(
+    linregsum$adj.r.squared,
+    lm_sum$adj.r.squared
+  )
+  expect_equal(
+    linregsum$fstatistic,
+    lm_sum$fstatistic
+  )
+  expect_equal(
+    linregsum$residuals,
+    lm_sum$residuals
+  )
+  expect_equal(
+    linregsum$df,
+    lm_sum$df
+  )
+})
+
+
+
+
+
+
